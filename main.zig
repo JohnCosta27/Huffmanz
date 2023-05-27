459b8d902d29c1d2fa1468beb455275d153563b4
John Costa <johncosta027@gmail.com>
2023-05-27 16:33:20 +0100

    Using mem.copy to get source array into my own

--- main.zig
+++ main.zig
@@ -1,24 +1,28 @@
 const std = @import("std");
+const mem = std.mem;
 
 const Heap = struct {
     array: []i32,
     allocator: std.mem.Allocator,
 
-    pub fn init(allocator: std.mem.Allocator) Heap {
+    pub fn init(allocator: mem.Allocator) Heap {
         return Heap{
             .array = allocator.alloc(i32, 1024),
             .allocator = allocator,
         };
     }
 
-    pub fn heapify(allocator: std.mem.Allocator, array: []i32) !void {
-        const memory = try allocator.alloc(i32, 1024);
+    pub fn heapify(allocator: mem.Allocator, array: []i32) !void {
+        const memory = try allocator.alloc(i32, 32);
 
-        _ = Heap{
+        const myHeap = Heap{
             .array = memory,
             .allocator = allocator,
         };
-        for (array) |item| {
+
+        mem.copy(i32, myHeap.array, array);
+
+        for (myHeap.array) |item| {
             std.debug.print("Element: {}\n", .{item});
         }
     }
