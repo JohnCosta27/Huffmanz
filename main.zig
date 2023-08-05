0519df17db40ae41be1fcfc809b9d8774e08985d
John Costa <johncosta027@gmail.com>
2023-08-05 14:19:24 +0100

    FEAT: Qing

--- main.zig
+++ main.zig
@@ -3,13 +3,15 @@ const mem = std.mem;
 
 const Queue = @import("priority_queue.zig");
 const PriorityQueue = Queue.PriorityQueue;
+const el = @import("element.zig");
+const Element = el.Element;
 
 pub fn main() !void {
     const page_alloc = std.heap.page_allocator;
     const myString = "Hello world";
 
     // Map between ASCII and frequency
-    var charMap = std.AutoHashMap(u8, u32).init(page_alloc);
+    var charMap = std.AutoHashMap(u8, i32).init(page_alloc);
 
     for (myString) |char| {
         const mapChar = charMap.get(char);
@@ -20,11 +22,15 @@ pub fn main() !void {
         }
     }
 
-    // var q = PriorityQueue.init(page_alloc);
-
+    var q = try PriorityQueue.init(page_alloc);
     var mapIter = charMap.iterator();
 
     while (mapIter.next()) |entry| {
-        std.debug.print("Key: {}, Value: {d}\n", .{ entry.key_ptr.*, entry.value_ptr.* });
+        // std.debug.print("Key: {c}, Value: {d}\n", .{ entry.key_ptr.*, entry.value_ptr.* });
+        const elArray = [_]u8{entry.key_ptr.*};
+        q.enqueue(Element{
+            .value = &elArray,
+            .priority = -entry.value_ptr.*,
+        });
     }
 }
