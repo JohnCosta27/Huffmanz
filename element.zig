3ebd633507116a5a77c02fa364a26786f7fd3643
John Costa <johncosta027@gmail.com>
2023-08-10 23:08:57 +0100

    FEAT: Tree structure

--- element.zig
+++
@@ -1,5 +0,0 @@
-pub const Element = struct {
-    // String
-    value: []const u8,
-    priority: i32,
-};
--- main.zig
+++ main.zig
@@ -1,16 +1,21 @@
 const std = @import("std");
 const mem = std.mem;
+const Order = std.math.Order;
 
-const Queue = @import("priority_queue.zig");
-const PriorityQueue = Queue.PriorityQueue;
-const el = @import("element.zig");
-const Element = el.Element;
+const h = @import("heap.zig");
+const Heap = h.Heap;
 
 pub const TreeNode = struct {
-    // String
-    value: []const u8,
-    priority: i32,
+    // char
+    value: u8,
+    probability: i32,
+    left_child: ?*TreeNode,
+    right_child: ?*TreeNode,
 };
+
+fn lessThanTree(A: TreeNode, B: TreeNode) Order {
+    return std.math.order(A.probability, B.probability);
+}
 
 pub fn main() !void {
     const page_alloc = std.heap.page_allocator;
@@ -28,15 +33,65 @@ pub fn main() !void {
         }
     }
 
-    var q = try PriorityQueue.init(page_alloc);
+    var heap = try Heap(TreeNode, lessThanTree).init(page_alloc);
     var mapIter = charMap.iterator();
 
     while (mapIter.next()) |entry| {
-        // std.debug.print("Key: {c}, Value: {d}\n", .{ entry.key_ptr.*, entry.value_ptr.* });
-        const elArray = [_]u8{entry.key_ptr.*};
-        q.enqueue(Element{
-            .value = &elArray,
-            .priority = -entry.value_ptr.*,
-        });
+        var node: TreeNode = TreeNode{
+            .value = entry.key_ptr.*,
+            .probability = entry.value_ptr.*,
+            .left_child = null,
+            .right_child = null,
+        };
+
+        heap.insert(node);
+    }
+
+    // At least 2 items in heap.
+    while (heap.pointer > 1) {
+        var left = heap.remove().?;
+        var right = heap.remove().?;
+        var newNode = TreeNode{
+            .value = 0,
+            .probability = left.probability + right.probability,
+            .left_child = &left,
+            .right_child = &right,
+        };
+
+        heap.insert(newNode);
+    }
+
+    // const nodePointer = heap.remove().?;
+
+    var firstChild = TreeNode{
+        .value = 5,
+        .probability = 5,
+        .left_child = null,
+        .right_child = null,
+    };
+
+    var secondChild = TreeNode{
+        .value = 10,
+        .probability = 10,
+        .left_child = null,
+        .right_child = null,
+    };
+    var root = TreeNode{
+        .value = 1,
+        .probability = 5,
+        .left_child = &secondChild,
+        .right_child = &firstChild,
+    };
+
+    traverse(&root);
+}
+
+fn traverse(node: ?*const TreeNode) void {
+    std.debug.print("{}\n", .{node.?.value});
+    if (node.?.left_child != null) {
+        traverse(node.?.left_child.?);
+    }
+    if (node.?.right_child != null) {
+        traverse(node.?.right_child.?);
     }
 }
